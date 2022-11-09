09b6acb1a9aa50f9c5ca110cdb5e1d25c8fc7ac0
UhaiMong <uhaimarma2016@gmail.com>
2022-11-09 19:14:30 +0600

    pagination and all data load

--- index.js
+++ index.js
@@ -21,18 +21,28 @@ async function run() {
     try {
         const collectionOfServices = client.db('ZeronOne').collection('ZeroOneData');
         app.get('/services', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size);
             const query = {};
             const cursor = collectionOfServices.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
+            const services = await cursor.skip(page*size).limit(size).toArray();
+            const count = await collectionOfServices.estimatedDocumentCount();
+            res.send({count,services});
         });
+        app.get('/allservice', async (req, res) => {
+            const query = {};
+            const cursor = collectionOfServices.find(query);
+            const allservice = await cursor.toArray();
+            res.send(allservice);
+        })
         //load by specific id
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await collectionOfServices.findOne(query);
             res.send(result);
-        })
+        });
     }
     finally {
         
